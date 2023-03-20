2467b02c9fff191103f7e554e04d15bcd80dfb3a
Maxdude15 <maxodude1022@gmail.com>
2023-03-20 10:23:38 -0400

    C# stuff done

---
+++ My project 1/Assets/test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    
+    private float speed;
+    
+
+    void Start()
+    {
+        print("test Start");
+    }
+
+    void Update()
+    {
+        print(message:"test");
+    }
+}
