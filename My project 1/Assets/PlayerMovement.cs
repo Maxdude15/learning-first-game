7567c001c483f669637949fb5c9858d6355290b4
Maxdude15 <maxodude1022@gmail.com>
2023-03-24 10:18:20 -0400

    did some script stuff

---
+++ My project 1/Assets/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+    public float speed;
+    
+    void Update()
+    {
+       transform.Translate(0, 0, speed); 
+    }
+}
